2a28b3ee2aae04164d6cc78893a8baa6b9a07a84
spacewolf75-debug <spacewolf75@gmail.com>
2025-10-16 02:53:18 +0200

    Delete benign-remoteEntry.js

--- benign-remoteEntry.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  try {
-    console.log("✅ POC-REMOTE-LOADED");
-    window.__POC_REMOTE = { loaded: true, ts: Date.now() };
-    try { document.documentElement.setAttribute('data-poc-remote','1'); } catch(e){}
-  } catch(e){ console.error('POC error', e && e.message); }
-})();
